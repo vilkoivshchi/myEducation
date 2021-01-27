f3694e6a436cefb7834a852fd25144c4eb1d6c29
vilkoivshchi <bugagans@gmail.com>
2021-01-28 00:36:17 +0300

    Task2

--- HomeWork-5/Task1/Program.cs
+++ HomeWork-5/Task1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Task1
@@ -11,21 +12,26 @@ namespace Task1
     /// Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
     /// а) без использования регулярных выражений;
     /// б) с использованием регулярных выражений.
+    /// Шмаков.
     /// </summary>
     class Program
     {
+        /// <summary>
+        /// Проверяет корректность ввода логина без регулярок
+        /// </summary>
+        /// <param name="login"></param>
+        /// <returns></returns>
         static bool CheckLoginWithoutRegex(string login)
         {
             char[] loginArray = login.ToCharArray();
             bool loginCorrect = false;
-            
+
             if ((loginArray.Length >= 2 && loginArray.Length <= 10) && ((loginArray[0] >= 'A' && loginArray[0] <= 'Z') || (loginArray[0] >= 'a' && loginArray[0] <= 'z')))
-
-
             {
                 for (int i = 1; i < loginArray.Length; i++)
                 {
-                    if ((loginArray[i] >= '0' && loginArray[i] <= '9') || (loginArray[i] >= 'A' && loginArray[i] <= 'Z') || (loginArray[0] >= 'a' && loginArray[i] <= 'z'))
+                    if ((loginArray[i] >= '0' && loginArray[i] <= '9') || (loginArray[i] >= 'A' && loginArray[i] <= 'Z') || (loginArray[i] >= 'a' && loginArray[i] <= 'z'))
+                        
                     {
                         loginCorrect = true;
                     }
@@ -35,21 +41,34 @@ namespace Task1
             return loginCorrect;
         }
 
+        /// <summary>
+        /// Проверяет корректность ввода логина c регулярками
+        /// </summary>
+        /// <param name="login"></param>
+        /// <returns></returns>
+        static bool CheckLoginWithRegex(string login)
+        {
+            bool loginCorrect = false;
+            Regex check = new Regex(@"^(?!\d)([A-Za-z0-9]{2,10})$");
+            if (check.IsMatch(login)) loginCorrect = true;
+            return loginCorrect;
+        }
+
         static void Main(string[] args)
         {
-            
-
-            Console.Write($"Введите логин: ");
-            string login = Console.ReadLine();
-            if (CheckLoginWithoutRegex(login))
+            while (true)
             {
-                Console.WriteLine($"login {login} удовлетворяет условиям задачи");
-                    }
-            else
-            {
-                Console.WriteLine($"login {login} НЕ удовлетворяет условиям задачи");
+                Console.Write($"Введите логин: ");
+                string login = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine($"Проверка логина без регулярок:");
+                Console.WriteLine(CheckLoginWithoutRegex(login) ? $"login {login} удовлетворяет условиям задачи" : $"login {login} НЕ удовлетворяет условиям задачи");
+                Console.WriteLine();
+                Console.WriteLine($"Проверка логина с регулярками:");
+                Console.WriteLine(CheckLoginWithRegex(login) ? $"login {login} удовлетворяет условиям задачи" : $"login {login} НЕ удовлетворяет условиям задачи");
             }
-            Console.ReadLine();
+            
+          
         }
     }
 }
---
+++ HomeWork-5/Task2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    /// <summary>
+    /// 2. Разработать класс Message, содержащий следующие статические методы для обработки текста:
+    /// а) Вывести только те слова сообщения, которые содержат не более n букв.
+    /// б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+    /// в) Найти самое длинное слово сообщения.
+    /// г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+    /// Продемонстрируйте работу программы на текстовом файле с вашей программой.
+    /// </summary>
+
+    class Message
+    {
+        /// <summary>
+        /// Массив для храниния строк
+        /// </summary>
+        public string[] message { get; set; }
+
+        
+
+        /// <summary>
+        /// Конструктор читает с диска файл и разделяет содержимое
+        /// </summary>
+        /// <param name="filename">Имя файла</param>
+        /// <param name="separators">Разделители</param>        
+        public Message(string filename, string[] separators)
+        {
+            if (File.Exists(filename))
+            {
+
+                StreamReader reader = new StreamReader(filename);
+                string textMessage = File.ReadAllText(filename);
+                
+                var parsedMessage = textMessage.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                message = new string[parsedMessage.Length];
+                for (int i = 0; i < parsedMessage.Length; i++)
+                {
+                    message[i] = parsedMessage[i];
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+            
+        }
+        // запилить поле _size;
+        public string[] WordLongerThen(string[] stringsArray, int size)
+        {
+            //size = _size;
+            string[] newArray = new string[stringsArray.Length];
+            int newArrayCount = 0;
+            foreach(string a in stringsArray)
+            {
+                if (a.Length <= size)
+                {
+                    newArray[newArrayCount] = a;
+                    newArrayCount++;
+                }
+            }
+            // выбросим лишнее
+            string[] finalArray = new string[newArrayCount];
+            for (int i = 0; i < finalArray.Length; i++)
+            {
+                finalArray[i] = newArray[i];
+            }
+            
+
+            return finalArray;
+        }
+    
+    }
+
+
+
+
+
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string filename = "Text.txt";
+            filename = AppDomain.CurrentDomain.BaseDirectory + filename;
+
+            string[] separators = { ",", ".", " ", "?", "!", ";", ":" };
+
+            int maxLetters = 4;
+
+            Message message = new Message(filename, separators);
+            string[] fromTextFile = message.message;
+
+            string[] wordsLessThen = message.WordLongerThen(fromTextFile, maxLetters);
+            Console.WriteLine($"Массив из файла:");
+            foreach (string a in fromTextFile) Console.WriteLine(a);
+            Console.WriteLine();
+            Console.WriteLine($"Теперь показываем слова не длиннее {maxLetters} букв:");
+            foreach (string a in wordsLessThen) Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
