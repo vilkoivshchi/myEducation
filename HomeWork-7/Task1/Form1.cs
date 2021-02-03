962069d34752c5b7b42fdf3981ca55a6f9dc8e83
vilkoivshchi <bugagans@gmail.com>
2021-02-04 00:08:05 +0300

    123

--- HomeWork-7/Task1/Form1.cs
+++ HomeWork-7/Task1/Form1.cs
@@ -18,9 +18,11 @@ namespace Task1
             button1.Text = "+1";
             button2.Text = "x2";
             button3.Text = "Сброс";
+            button4.Text = "Играть";
             label1.Text = "0";
             label2.Text = "0";
             this.Text = "Удвоитель";
+
         }
 
 
@@ -38,8 +40,75 @@ namespace Task1
 
         private void button3_Click(object sender, EventArgs e)
         {
-            label1.Text = "1";
-            label2.Text = (int.Parse(label2.Text) + 1).ToString();
+            label1.Text = "0";
+            label2.Text = "0";
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string caption = "Я загадал число";
+            
+            string message = numbers.number.ToString();
+            
+            label1.Text = "0";
+            label2.Text = "0";
+
+            MessageBox.Show(message, caption);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public delegate void SomeNumbersHandler(object sender, SomeNumbers e);
+       public class SomeNumbers
+        {
+            public int number { get; }
+            public int answer { get; }
+            public SomeNumbers()
+            {
+                int[] finalArr = new int[2];
+                
+                Random rand = new Random();
+
+                number = rand.Next(50, 500);
+                
+                int answer = 0;
+                
+                // Поищем наименьшее число ходов
+                while (number > 0)
+                {
+                    if (number % 2 == 0)
+                    {
+                        number /= 2;
+                        answer++;
+                    }
+                    else
+                    {
+                        number--;
+                        answer++;
+                    }
+
+                }
+
+            }
+            /*
+            public static int CheckAnswer(int a)
+            {
+               
+            }
+            */
         }
     }
 }
--- HomeWork-7/Task1/Program.cs
+++ HomeWork-7/Task1/Program.cs
@@ -23,6 +23,7 @@ namespace Task1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
